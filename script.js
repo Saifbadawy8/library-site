726b1574de23ea7dde1be55a1ccc1c82070a845b
seif <seif@seifs-MacBook-Pro.local>
2025-05-17 07:07:10 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function handleSearch() {
+    const query = document.getElementById('searchBox').value;
+    console.log(`Searching for: ${query}`);
+    // You can add actual search logic here
+    <button onclick="window.location.href='newpage.html'" class="nav-button">Go to New Page</button>
+
+}
